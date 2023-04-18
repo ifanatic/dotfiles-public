b78942f0957a1e705e4c43131f31a6060a93e581
ifanatic <onlk@yandex.ru>
2023-04-18 22:12:55 +0500

    (refactor) Some comments added

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,16 +1,23 @@
 return require('packer').startup(function(use)
-	use 'airblade/vim-gitgutter'
+	-- Plugins
+    use 'airblade/vim-gitgutter'
 	use 'machakann/vim-highlightedyank'
 	use 'andymass/vim-matchup'
 	use 'tpope/vim-commentary'
 	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+
+    -- Navigation
 	use 'justinmk/vim-sneak'
 	use 'nvim-lua/plenary.nvim'
 	use 'nvim-telescope/telescope.nvim'
 	use 'nvim-telescope/telescope-file-browser.nvim'
+
+    -- UI
 	use 'romgrk/barbar.nvim'
 	use 'nvim-tree/nvim-tree.lua'
 	use 'nvim-lualine/lualine.nvim'
+
+    -- LSP
 	use 'rust-lang/rust.vim'
 	use 'neovim/nvim-lspconfig'
 	use 'nvim-lua/lsp_extensions.nvim'
@@ -19,6 +26,8 @@ return require('packer').startup(function(use)
 	use 'hrsh7th/cmp-nvim-lsp'
 	use 'hrsh7th/cmp-buffer'
 	use 'hrsh7th/nvim-cmp'
+
+    -- Themes
 	use 'rakr/vim-one'
 end)
 
